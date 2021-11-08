e9abc52e36f92f37bd34feba206a554b80100748
Cadeusept <matwey.likhota@gmail.com>
2021-11-08 14:06:47 +0300

    [[maybe_wrong]]

--- include/sharedptr.hpp
+++ include/sharedptr.hpp
@@ -8,25 +8,90 @@
 template <typename T>
 class SharedPtr {
 public:
-    SharedPtr();
-    SharedPtr(T* ptr);
-    SharedPtr(const SharedPtr& r);
-    SharedPtr(SharedPtr&& r);
-    ~SharedPtr();
-    auto operator=(const SharedPtr& r) -> SharedPtr&;
-    auto operator=(SharedPtr&& r) -> SharedPtr&;
+    SharedPtr() {
+      this->_ptr = nullptr;
+      this->_count = 0;
+    }
+
+    SharedPtr(T* ptr) {
+      this->_ptr = ptr;
+      this->_count = 1;
+    }
+
+    SharedPtr(const SharedPtr& r) {
+      this->_ptr = r.ptr;
+      this->_count = r.use_count();
+      this->_count++;
+    }
+
+    SharedPtr(SharedPtr&& r) {
+      this->_ptr = *r.get();
+      this->_count = *r.use_count();
+      this->count++;
+    }
+    ~SharedPtr() {
+      this->_count--;
+      if ((this->_count == 0) && this) {
+        delete this->_ptr;
+      }
+    }
+
+    auto operator=(const SharedPtr& r) -> SharedPtr& {
+      _ptr=r.get();
+      _count=r.use_count();
+    }
+
+    auto operator=(SharedPtr&& r) -> SharedPtr& {
+      _ptr=*r.get();
+      _count=*r.use_count();
+    }
 
     // проверяет, указывает ли указатель на объект
-    operator bool() const;
-    auto operator*() const -> T&;
-    auto operator->() const -> T*;
+    operator bool() const {
+      if (this->_ptr == nullptr)
+        return false;
+      else
+        return true;
+    }
 
-    auto get() -> T*;
-    void reset();
-    void reset(T* ptr);
-    void swap(SharedPtr& r);
+    auto operator*() const -> T& {
+      return *this->_ptr;
+    }
+
+    auto operator->() const -> T* {
+      return this->_ptr;
+    }
+
+    auto get() -> T* {
+      return this->_ptr;
+    }
+
+
+
+    void reset() {
+      this->_count--;
+      this->_ptr = nullptr;
+    }
+
+    void reset(T* ptr) {
+      this->_ptr = ptr;
+      this->_count = 1;
+    }
+
+    void swap(SharedPtr& r) {
+      T* tmp_ptr = r.get();
+      size_t tmp_count = r.use_count();
+
+      r = this;
+
+      this->_ptr = tmp_ptr;
+      this->_count = tmp_count;
+    }
+
     // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
-    auto use_count() const -> size_t;
+    auto use_count() const -> size_t {
+      return _count;
+    }
 
 private:
     T* _ptr;
